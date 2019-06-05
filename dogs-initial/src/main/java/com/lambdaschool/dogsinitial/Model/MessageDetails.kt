2d6649262fe792dda0e8f40d8bc47206634e3544
Jake Esveld <thejakeesveld@gmail.com>
2019-06-05 12:53:51 -0700

    Add rabbit MQ listener and handle receiving messages

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Model/MessageDetails.kt
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/Model/MessageDetails.kt
@@ -2,8 +2,10 @@ package com.lambdaschool.dogsinitial.Model
 
 import java.io.Serializable
 
-class MessageDetails(var text: String, var priority: Int, var secret: Boolean): Serializable{
+class MessageDetails(var text: String = "", var priority: Int = 1, var secret: Boolean = true): Serializable{
     override fun toString(): String {
         return "MessageDetails(text='$text', priority=$priority, secret=$secret)"
     }
+
+
 }
